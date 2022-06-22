6a6b30745b156880da87cecc1ce94629d2726a98
toghrul-nasirli <toghrulnasirli111@gmail.com>
2022-06-22 16:08:00 +0400

    minor changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,6 @@ import Swal from "sweetalert2";
 import TinyMCE from "tinymce";
 
 try {
-    // window.Popper = require('@popperjs/core').default;
-    // require("bootstrap");
-    
     window.$ = window.jQuery = Jquery;
     window.Swal = Swal;
     window.tinymce = TinyMCE;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,14 +8,14 @@
 <body class="theme-{{ $darkmode ? 'dark' : 'light' }}">
     <div id="app">
         @include('partials._sidebar')
-        
+
         <div id="main">
             <header class="mb-3">
                 <a href="javascript:void(0)" class="burger-btn d-block d-xl-none">
                     <i class="bi bi-justify fs-3"></i>
                 </a>
             </header>
-            
+
             @yield('content')
 
             @include('partials._footer')
